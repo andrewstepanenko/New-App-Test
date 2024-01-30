1b873ac75cceabb8686516d84a03398d3f4980ba
Andrew Stepanenko <maclover7474@gmail.com>
2024-01-31 01:36:26 +0200

    Local Push Notifications Added

--- Do Test/ContentView.swift
+++ Do Test/ContentView.swift
@@ -6,19 +6,33 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "person.fill")
-                .font(.system(size: 30, weight: .bold))
-                .foregroundColor(.black.opacity(0.65))
-            Text("Hello, user!")
-                .font(.system(size: 30, weight: .bold))
-            
-            Spacer()
+        ZStack {
+            Color("bgColor").ignoresSafeArea()
+            VStack(spacing: 30) {
+                    VStack(spacing: 5) {
+                        Image(systemName: "person.fill")
+                            .font(.system(size: 30, weight: .bold))
+                        Text("Hello, user!")
+                            .font(.system(size: 30, weight: .bold))
+                    }
+                    
+                    Button {
+                        checkForPermisson()
+                    } label: {
+                        requestNotificationsBtn()
+                    }
+                }
+                .padding(20)
+                .background(Color.black.opacity(0.075))
+                .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                .frame(height: 200)
+                
+                //.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
         }
-        .padding(.top, 50)
     }
 }
 
@@ -26,4 +40,67 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct requestNotificationsBtn: View {
+    var body: some View {
+        VStack {
+            Text("Receive notifications!")
+                .font(.system(size: 20, weight: .semibold))
+                .foregroundColor(.white)
+        }
+        .frame(width: 250, height: 50)
+        .background(Color.gray)
+        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+        .shadow(color: Color("shadowColor").opacity(0.1), radius: 7, x: 0, y: 12)
+        .shadow(color: .black.opacity(0.1), radius: 1, x: 0, y: 1)
+    }
+}
+
+func checkForPermisson() {
+    let notificationCenter = UNUserNotificationCenter.current()
+    notificationCenter.getNotificationSettings { settings in
+        switch settings.authorizationStatus {
+        case .authorized:
+            dispatchNotifications()
+        case .notDetermined:
+            notificationCenter.requestAuthorization(options: [.alert, .sound]) {didAllow, error in
+                if didAllow {
+                    dispatchNotifications()
+                }
+            }
+        case .denied:
+            return
+
+         default:
+            return
+        }
+    }
+}
+
+func dispatchNotifications() {
+    let identifier = "first-notification"
+    let title = "Hello, it's your to-do App!"
+    let body = "Time to manage some stuff..."
+    let hour = 13
+    let minute = 30
+    let isDaily = true
+    
+    let notificationCenter = UNUserNotificationCenter.current()
+    
+    let content = UNMutableNotificationContent()
+    content.title = title
+    content.body = body
+    content.sound = .default
+    
+    let calendar = Calendar.current
+    var dateComponents = DateComponents(calendar: calendar, timeZone: TimeZone.current)
+    dateComponents.hour = hour
+    dateComponents.minute = minute
+    
+    let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: isDaily)
+    let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+    
+    notificationCenter.removePendingNotificationRequests(withIdentifiers: [identifier])
+    notificationCenter.add(request)
 }
